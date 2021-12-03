10724d54a9f5ee3b13ba06ab3115c656a320d248
belanglos <benjamin@longstream.se>
2021-12-03 14:24:16 +0100

    Remove postinstaller from copied files.

--- setupDirectoriesAndScriptsForVSCodiumForCora.sh
+++ setupDirectoriesAndScriptsForVSCodiumForCora.sh
@@ -32,9 +32,6 @@ changeAndCopyScripts(){
 	sed -i "s|INSTALLDIR|$INSTALLDIR|g" $INSTALLDIR/startVSCodiumForCoraTempSetup.sh
 	sed -i "s|PARENTDIR|$PARENTDIR|g" $INSTALLDIR/startVSCodiumForCoraTempSetup.sh
 
-	cp $BASEDIR/postInstaller.sh $INSTALLDIR/
-	chmod +x $INSTALLDIR/postInstaller.sh
-
 	# cp $BASEDIR/startVSCodiumForCoraNoPorts.sh $INSTALLDIR/
 	# sed -i "s|INSTALLDIR|$INSTALLDIR|g" $INSTALLDIR/startVSCodiumForCoraNoPorts.sh
 	# sed -i "s|PARENTDIR|$PARENTDIR|g" $INSTALLDIR/startVSCodiumForCoraNoPorts.sh
